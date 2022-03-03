ec04222e7013674e6adc4fca7da63fca8dfd9d1e
kushikarthik <99874027+kushikarthik@users.noreply.github.com>
2022-03-03 23:26:06 +0530

    Add files via upload

---
+++ phase1/Assisted/BinarySearch.java
@@ -0,0 +1,52 @@
+package AsstPraticeProjects;
+
+
+	import java.util.Scanner;
+
+	public class BinarySearch {
+
+		public static void main(String[] args) {
+
+			int arr[] = { 1, 3, 5, 7, 9, 11 };
+
+			int length = arr.length;
+
+			int start = 0;
+
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("Enter key for Binary search");
+			int key = scanner.nextInt();
+			
+				binarySearch(arr, start, key, length);
+
+		}
+		
+		
+
+	public static void binarySearch(int[] arr, int start, int key, int length){
+
+	        int midValue = (start+length)/2;
+	        while(start<=length){
+
+	            if(arr[midValue]<key){
+
+	                start = midValue + 1;
+	            } else if(arr[midValue]==key){
+	                System.out.println("Element is found at index :"+midValue);
+	                break;
+	            }else {
+
+	                length=midValue-1;
+	            }
+	            midValue = (start+length)/2;
+	        }
+	            if(start>length){
+
+	                System.out.println("Element is not found");
+	            }
+
+	}
+
+	}
+
