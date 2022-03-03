c12097505103de10f07992bd939df806a4792dd0
kushikarthik <99874027+kushikarthik@users.noreply.github.com>
2022-03-03 23:40:36 +0530

    Add files via upload

---
+++ phase1/Assisted/ExponentialSearch.java
@@ -0,0 +1,49 @@
+package AsstPraticeProjects;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ExponentialSearch {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 7, 4, 44, 45, 12, 25, 52, 91 };
+		int length = arr.length;
+
+		System.out.println("Enter key for Exponential search");
+
+		Scanner scanner = new Scanner(System.in);
+		int key = scanner.nextInt();
+
+		int outcome = exponentialSearch(arr, length, key);
+
+		if (outcome < 0) {
+
+			System.out.println("Element not found");
+
+		} else {
+
+			System.out.println("Element found at index " + outcome);
+		}
+
+	}
+
+	public static int exponentialSearch(int arr[], int length, int key) {
+
+		if (arr[0] == key) {
+			return 0;
+		}
+		int i = 1;
+		while (i < length && arr[i] <= key) {
+
+			i = i * 2;
+			break;
+		}
+
+		return Arrays.binarySearch(arr, i / 2, Math.min(i, length), key);
+
+	}
+
+}
+
+
